a1aa25d463bfc36b3e061de19b24ce0dd0db3461
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2022-10-10 11:58:56 +0700

    Mengizinkan CORS dari semua sumber

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ import express from "express";
 import router from "./routes/routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 
 try {
     await db.authenticate();
